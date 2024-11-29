19794150ac9d11d006ed8f0e8fa59a99a9d5bfae
HAEMIC <Michael.Haemmerle@illwerkevkw.at>
2024-11-29 08:19:17 +0100

    simons push

---
+++ ArrowLog/src/Database/DbContext.cs
@@ -0,0 +1,25 @@
+﻿using ArrowLog.Features.Login;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace ArrowLog.Database;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<User> Blogs { get; set; }
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "arrowlog.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
---
+++ ArrowLog/src/Features/Login/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ArrowLog.Features.Login;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
+
