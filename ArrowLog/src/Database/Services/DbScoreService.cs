90c9bc8e8bb61f5f8e4def1cec8c9f08e0f2de9a
HAEMIC <Michael.Haemmerle@illwerkevkw.at>
2024-12-13 10:44:27 +0100

    removed templates

---
+++ ArrowLog/src/Database/Services/DbScoreService.cs
@@ -0,0 +1,34 @@
+﻿using ArrowLog.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ArrowLog.Database.Services;
+
+public class DbScoreService
+{
+    private AppDbContext _context;
+
+    public DbScoreService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    /*
+    public Score CreateScore(Score score)
+    {
+        try
+        {
+
+            await _context.Persons.AddAsync(person);
+
+            await _context.SaveChangesAsync();
+
+            return person;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    } 
+    */
+}
