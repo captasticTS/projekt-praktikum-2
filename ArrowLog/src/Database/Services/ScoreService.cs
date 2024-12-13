51820d53502432fee3ef8eff1a0586d5bd6c4b7c
HAEMIC <Michael.Haemmerle@illwerkevkw.at>
2024-12-13 11:13:50 +0100

    added DbGameService and DbScoreService

---
+++ ArrowLog/src/Database/Services/ScoreService.cs
@@ -0,0 +1,78 @@
+﻿using ArrowLog.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ArrowLog.Database.Services;
+
+public class ScoreService
+{
+    private AppDbContext _context;
+
+    public ScoreService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Score?> CreateScore(Score score)
+    {
+        try
+        {
+            await _context.Scores.AddAsync(score);
+
+            await _context.SaveChangesAsync();
+
+            return score;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<Score?> UpdateScore(Score score)
+    {
+        try
+        {
+            var existingScore = await _context.Scores.FindAsync(score.Id);
+
+            if (existingScore is null)
+            {
+                return null;
+            }
+
+            existingScore.TotalScore = score.TotalScore;
+
+            await _context.SaveChangesAsync();
+
+            return existingScore;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public async Task<bool> DeleteScore(Score score)
+    {
+        try
+        {
+            var existingScore = await _context.Scores.FindAsync(score.Id);
+
+            if (existingScore is null)
+            {
+                return false;
+            }
+
+            _context.Scores.Remove(existingScore);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
