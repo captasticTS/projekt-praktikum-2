3ee94ae475a0a07b2401c1fa5dcf05571aec51f1
aau27722 <aau27722@CAU2685.net.blum>
2024-12-13 08:43:50 +0100

    Added Databases

---
+++ ArrowLog/src/Database/Models/Parcours.cs
@@ -0,0 +1,11 @@
+﻿namespace ArrowLog.Database.Models;
+
+public class Parcours
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public int AnimalCount { get; set; }
+    public List<Game> Games { get; set; } = new();
+}
+
