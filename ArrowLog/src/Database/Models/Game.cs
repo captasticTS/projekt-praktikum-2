6b5291bb4bdaeeed8c4b630ea05fc6c26202131f
bau21982 <simon.bereuter@blum.com>
2025-01-10 10:48:15 +0100

    added Functionallity to join and create Game

--- ArrowLog/src/Database/Models/Game.cs
+++ ArrowLog/src/Database/Models/Game.cs
@@ -9,4 +9,5 @@ public class Game
     public List<Score> Scores { get; set; } = new();
     public Ruleset Ruleset { get; set; } = new();
     public Person Owner { get; set; } = new();
+    public Parcours Parcours { get; set; } = new();
 }
